0f77afaf846c2e2905ecbadd9a61e06e678855fb
Ujjawal_Tyagi <tyagiujjawal9@gmail.com>
2023-07-15 18:08:56 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import React from "react";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { StateContextProvider } from "./context";
+import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+      <Router>
+        <StateContextProvider>
+          <App />
+        </StateContextProvider>
+      </Router>
+    </ThirdwebProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
